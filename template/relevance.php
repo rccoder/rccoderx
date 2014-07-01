9be3e9c89b4b7d5f7fd83d1429903229e9b51feb
ysbinang <ysbinang@qq.com>
2014-07-02 00:48:58 +0800

    Revert "change"
    This reverts commit 1ef7c940a423ece5443e0c40a5ab1e082067ecc2.

--- template/relevance.php
+++ template/relevance.php
@@ -17,14 +17,7 @@
 			$post_tag = $tag_list[ rand(0, count($tag_list) - 1) ];
 			$query_posts = new WP_Query('orderby=rand&caller_get_posts=1&showposts=4&tag_in='.$post_tag);
 		?>
-		<?php if($query_posts->have_posts()):while($query_posts->have_posts()):$query_posts->the_post(); 
-		$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-		$args = array(
-    		// 控制只显示10篇文章，如果将10改成-1将显示所有文章
-    	'showposts' => 12,
-    	'paged' => $paged
-		);
-		query_posts($args);?>
+		<?php if($query_posts->have_posts()):while($query_posts->have_posts()):$query_posts->the_post(); ?>
 		<dl>
 			<dt><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></dt>
 			<dd>Posted on <?php the_time('m月d日'); ?></dd>
